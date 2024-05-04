628fccd7bfe334f3dd1e9bb9ecbeae4cee85f2b6
Ian Perfitt <iancp@protonnmail.com>
2024-05-04 09:05:27 -0500

    finished practicing loops

--- GroovyTut.groovy
+++ GroovyTut.groovy
@@ -124,9 +124,35 @@ class GroovyTut {
         i++;
         continue;
       }
+      if(i == 8) {
+        break;
+      }
+      println(i);
+      i++;
     }
 
+    for(i = 0; i < 5; i++) {
+      println(i);
+    }
 
+    for(j in 2..6) {
+      println(j);
+    }
+
+    def randList = [10, 12, 13, 14];
+    for(j in randList) {
+      println(j);
+    }
+
+    def = [
+      100 : "Paul",
+      101 : "Sally",
+      102 : "Sue"
+    ];
+
+    for(cust in custs) {
+      println("$cust.value : $cust.key");
+    }
 
   }
 }
